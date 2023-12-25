2d0fafdc11c12f2543e1c2ab794b3df56b8db98b
NogaTal98 <isanyo.noga@gmail.com>
2023-12-25 11:48:25 +0200

    header
    add logo and question mark icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,22 @@
+import React from "react";
+import logo from "../resources/wordle-logo.png";
+import question from "../resources/question-mark.png";
+
 function Header() {
     return (
-      <div className="header">
-      </div>
+        <div className="header">
+            <div className="header-section">
+
+            </div>
+            <div className="header-section">
+                <img src={logo} alt="Logo" className="logo"/>
+            </div>
+            <div className="header-section header-right">
+                <img src={question} alt="Question" className="question"/>
+            </div>
+            {/* 
+            <img src={question} alt="Question" className="question"/> */}
+        </div>
     );
   }
 
