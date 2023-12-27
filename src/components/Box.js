04ab5a041d8bc5a234dc32a238cea3cf43ebd75e
NogaTal98 <isanyo.noga@gmail.com>
2023-12-27 13:16:13 +0200

    arange back and front card css
    fix the css so the coloer of the cerd will be only on it's back and will reveal after the flip

--- src/components/Box.js
+++ src/components/Box.js
@@ -2,7 +2,7 @@ function Box({value}) {
     return (
         <div className="flip-box" >
             <div className="flip-box-inner">
-                <div className={"box " + value[1] + " flip-box-front"}>
+                <div className={"box " + (value[0] !== "" ? "filled" : "") + " flip-box-front"}>
                     {value[0]}
                 </div>
                 <div className={"box " + value[1] + " flip-box-back"}>
